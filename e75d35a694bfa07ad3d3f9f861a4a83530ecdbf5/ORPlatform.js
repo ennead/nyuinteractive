0b496c11453179f9c3c63620a47025c56891eea9
Cesar Neri <cneri@andrew.cmu.edu>
2019-05-13 09:32:53 -0400

    implemented new styling to some pages of phase 1

--- e75d35a694bfa07ad3d3f9f861a4a83530ecdbf5/ORPlatform.js
+++ e75d35a694bfa07ad3d3f9f861a4a83530ecdbf5/ORPlatform.js
@@ -1,10 +1,14 @@
 var h = window.innerHeight;
-var sectionduration = h/2;
+var sectionduration = h/3;
 var s1location = 0;
 var s2location = sectionduration;
 var s3location = sectionduration*2;
 var s4location = sectionduration*3;
 var backDiagramURL;
+var hotspotActive = false;
+
+var totalItems = $('.carousel-item').length;
+var currentIndex = $('div.active').index()+1;
 
 
 $(function () { // wait for document ready
@@ -16,20 +20,23 @@ $(function () { // wait for document ready
 
   // show pin state
 
-function updateBox0(){
-   $("#slideCounter").text("01");
-   $("#title").text("A highly flexible and efficient platform.");
-   $("#diagramBack").attr('src', backDiagramURL);
-   $("#diagramFront").attr('src', 'assets/diagrams/OR Platform/fullHighlight.png').stop(true,true).hide().fadeIn("slow");
-   $("#sectionBody").text("The new tower houses 32 operating and procedure suites located on three floors. All staff support spaces and sterile processing are centralized off the floor, to maximize the efficiency of the planning.");
-   backDiagramURL = 'assets/diagrams/OR Platform/fullHighlight.png';
-   $("#pl2,#pl3,#pl4").removeClass("activeLink").addClass("progressLink")
-   $("#pl1").addClass("activeLink")
-   $('#prevSection').click( function(e) {e.preventDefault(); controller.scrollTo(s1location-1); return false; } );
-   $('#nextSection').click( function(e) {e.preventDefault(); controller.scrollTo(s2location-1); return false; } );
-   $('#prevSection').css('opacity', '0.2');
-   $('#nextSection').css('opacity', '1');
-}
+  function updateBox0 () {
+
+    $("#slideCounter").text("01");
+    $("#title").text("A highly flexible and efficient platform.");
+    $("#diagramBack").attr('src', backDiagramURL);
+    $("#diagramFront").attr('src', 'assets/diagrams/OR Platform/fullHighlight.png').stop(true,true).hide().delay(200).fadeIn("slow");
+    $("#sectionBody").text("The new tower houses 32 operating and procedure suites located on three floors. All staff support spaces and sterile processing are centralized off the floor, to maximize the efficiency of the planning.");
+    backDiagramURL = 'assets/diagrams/OR Platform/fullHighlight.png';
+    $("#s2,#s3,#s4").removeClass("activeStep");
+    $("#s1").addClass("activeStep");
+    $('#prevSection').click( function(e) {e.preventDefault(); controller.scrollTo(s1location-1); return false; } );
+    $('#nextSection').click( function(e) {e.preventDefault(); controller.scrollTo(s2location-1); return false; } );
+    $('#prevSection').css('opacity', '0.2');
+    $('#nextSection').css('opacity', '1');
+
+  }
+
 
   function updateBox1 (e) {
 
@@ -42,17 +49,17 @@ function updateBox0(){
       $("#diagramFront").attr('src', 'assets/diagrams/OR Platform/entrance.png').stop(true,true).hide().delay(200).fadeIn("slow");
       $("#sectionBody").text("The surgical reception and waiting areas are located on a series of balconies, open to the atrium and the lobby below. Patients and families arriving for a procedure can see where they are going and, once in the waiting room, where they have been.");
       backDiagramURL = 'assets/diagrams/OR Platform/entrance.png';
-      $("#pl1,#pl3,#pl4").removeClass("activeLink").addClass("progressLink")
-      $("#pl2").addClass("activeLink")
+      $("#s1,#s3,#s4").removeClass("activeStep");
+      $("#s2").addClass("activeStep");
       $('#prevSection').click( function(e) {e.preventDefault(); controller.scrollTo(s1location-1); return false; } );
       $('#nextSection').click( function(e) {e.preventDefault(); controller.scrollTo(s3location-1); return false; } );
       $('#prevSection').css('opacity', '1');
       $('#nextSection').css('opacity', '1');
     }
 
-  else if (scrollPos<1) {
-    updateBox0 ();
-  }
+    else if (scrollPos<1) {
+      updateBox0 ();
+    }
 
   }
 
@@ -64,8 +71,8 @@ function updateBox0(){
       $("#diagramFront").attr('src', 'assets/diagrams/OR Platform/blueHighlights.png').stop(true,true).hide().delay(200).fadeIn("slow");
       $("#sectionBody").text("To maximize utilization, all periop bays can be used interchangeably for patient prep and recovery. At the end of the day, as the census declines, the unit can contract to a smaller footprint.");
       backDiagramURL = 'assets/diagrams/OR Platform/blueHighlights.png';
-      $("#pl1,#pl2,#pl4").removeClass("activeLink").addClass("progressLink")
-      $("#pl3").addClass("activeLink")
+      $("#s1,#s2,#s4").removeClass("activeStep");
+      $("#s3").addClass("activeStep");
       $('#prevSection').click( function(e) {e.preventDefault(); controller.scrollTo(s2location-1); return false; } );
       $('#nextSection').click( function(e) {e.preventDefault(); controller.scrollTo(s4location-1); return false; } );
       $('#prevSection').css('opacity', '1');
@@ -81,14 +88,15 @@ function updateBox0(){
       $("#diagramFront").attr('src', 'assets/diagrams/OR Platform/orangeHighlight.png').stop(true,true).hide().delay(200).fadeIn("slow");
       $("#sectionBody").text("To maximize long-term flexibility, all image-guided labs are designed as hybrid ORs and located behind the red line, organized around a common clean core. Even the restricted corridors surrounding the ORs have access to daylight and views. Physicians and staff can step outside the ORs and make use of touch-down and respite space overlooking the East River.");
       backDiagramURL = 'assets/diagrams/OR Platform/orangeHighlight.png';
-      $("#pl1,#pl2,#pl3").removeClass("activeLink").addClass("progressLink")
-      $("#pl4").addClass("activeLink")
-      $('#prevSection').click( function(e) {e.preventDefault(); controller.scrollTo(s3location-10); return false; } );
-      $('#nextSection').click( function(e) {e.preventDefault(); controller.scrollTo(s2location-1); return false; } );
+      $("#s1,#s2,#s3").removeClass("activeStep");
+      $("#s4").addClass("activeStep");
+      $('#prevSection').click( function(e) {e.preventDefault(); controller.scrollTo(s3location-1); return false; } );
+      $('#nextSection').click( function(e) {e.preventDefault(); controller.scrollTo(s4location-1); return false; } );
       $('#prevSection').css('opacity', '1');
       $('#nextSection').css('opacity', '0.2');
     }
   }
+
 
 $('#prevSection').click( function(e) {e.preventDefault(); controller.scrollTo(s1location-1); return false; } );
 $('#nextSection').click( function(e) {e.preventDefault(); controller.scrollTo(s2location-1); return false; } );
@@ -97,25 +105,28 @@ $('#pl1').click( function(e) {e.preventDefault(); controller.scrollTo(s1location
 $('#pl2').click( function(e) {e.preventDefault(); controller.scrollTo(s2location-1); return false; } );
 $('#pl3').click( function(e) {e.preventDefault(); controller.scrollTo(s3location-1); return false; } );
 $('#pl4').click( function(e) {e.preventDefault(); controller.scrollTo(s4location-1); return false; } );
+// build scenes
+new ScrollMagic.Scene({duration: sectionduration, triggerHook: 0})
+  .setPin("#block")
+  .on("enter leave", updateBox1)
+  // .addIndicators() // add indicators (requires plugin)
+  .addTo(controller);
+
+new ScrollMagic.Scene({duration: sectionduration, offset: sectionduration, triggerHook: 0})
+  .setPin("#block")
+  .on("enter leave", updateBox2)
+  // .addIndicators() // add indicators (requires plugin)
+  .addTo(controller);
+
+new ScrollMagic.Scene({duration: sectionduration, offset: (sectionduration*2), triggerHook: 0})
+  .setPin("#block")
+  .on("enter leave", updateBox3)
+  // .addIndicators() // add indicators (requires plugin)
+  .addTo(controller);
+  });
 
 
-  // build scenes
-  new ScrollMagic.Scene({duration: sectionduration, triggerHook: 0})
-    .setPin("#block")
-    .on("enter leave", updateBox1)
-    // .addIndicators() // add indicators (requires plugin)
-    .addTo(controller);
-
-  new ScrollMagic.Scene({duration: sectionduration, offset: sectionduration, triggerHook: 0})
-    .setPin("#block")
-    .on("enter leave", updateBox2)
-    // .addIndicators() // add indicators (requires plugin)
-    .addTo(controller);
-
-  new ScrollMagic.Scene({duration: sectionduration, offset: (sectionduration*2), triggerHook: 0})
-    .setPin("#block")
-    .on("enter leave", updateBox3)
-    // .addIndicators() // add indicators (requires plugin)
-    .addTo(controller);
-
+$( window ).resize(function() {
+  diagramFrontWidth = $("#diagramFront").width()
+  diagramFrontHeight = $("#diagramFront").height();
 });
--- e75d35a694bfa07ad3d3f9f861a4a83530ecdbf5/clinicalExperience.js
+++ e75d35a694bfa07ad3d3f9f861a4a83530ecdbf5/clinicalExperience.js
@@ -1,12 +1,16 @@
 var h = window.innerHeight;
-var sectionduration = (h/3)*2;
+var sectionduration = h/3;
 var s1location = 0;
 var s2location = sectionduration;
 var s3location = sectionduration*2;
 var s4location = sectionduration*3;
 var s5location = sectionduration*4;
 var backDiagramURL;
-var learnMoreLink;
+var hotspotActive = false;
+
+var totalItems = $('.carousel-item').length;
+var currentIndex = $('div.active').index()+1;
+
 
 $(function () { // wait for document ready
   // init controller
@@ -18,12 +22,13 @@ $(function () { // wait for document ready
   // show pin state
 
   function updateBox0 () {
+
     $("#slideCounter").text("01");
     $("#title").text("Overview");
     $("#diagramBack").attr('src', backDiagramURL);
-    $("#diagramFront").attr('src', 'assets/diagrams/Section/fullsection.png').stop(true,true).hide().delay(200).fadeIn("slow");
+    $("#diagramFront").attr('src', 'assets/diagrams/Section/01.png').stop(true,true).hide().delay(200).fadeIn("slow");
     $("#sectionBody").text("The Helen L. and Martin H. Kimmel Pavilion is an 830,000-sf new inpatient tower, focused on the highest acuity patients and the most technological advanced care.  The new tower is integrated with the mid-century Tisch Hospital building--the combined facility is designed to function as a single inpatient environment of 550 all-private patient rooms.");
-    backDiagramURL = 'assets/diagrams/Section/fullsection.png';
+    backDiagramURL = 'assets/diagrams/Section/01.png';
     $("#learnMore").removeClass("learnMoreActive").addClass("learnMoreHidden");
     $("#s2,#s3,#s4,#s5").removeClass("activeStep");
     $("#s1").addClass("activeStep");
@@ -43,9 +48,9 @@ $(function () { // wait for document ready
       $("#slideCounter").text("02");
       $("#title").text("Public Space and Amenities");
       $("#diagramBack").attr('src', backDiagramURL);
-      $("#diagramFront").attr('src', 'assets/diagrams/Section/public_section.png').stop(true,true).hide().delay(200).fadeIn("slow");
+      $("#diagramFront").attr('src', 'assets/diagrams/Section/02.png').stop(true,true).hide().delay(200).fadeIn("slow");
       $("#sectionBody").text("The entry and atrium of the Kimmel Pavilion is connected to the lobby of Tisch hospital by a broad public concourse, lined with patient and staff services and retail amenities. Additional public amenities are found on the seventh floor, including a major roof terrace and garden overlooking the East River.");
-      backDiagramURL = 'assets/diagrams/Section/public_section.png';
+      backDiagramURL = 'assets/diagrams/Section/02.png';
       $("#learnMore").removeClass("learnMoreHidden").addClass("learnMoreActive");
       $("#learnMoreLink").attr('href', 'publicSpace.html');
       $("#s1,#s3,#s4,#s5").removeClass("activeStep");
@@ -67,9 +72,9 @@ $(function () { // wait for document ready
       $("#slideCounter").text("03");
       $("#title").text("OR Platform");
       $("#diagramBack").attr('src', backDiagramURL);
-      $("#diagramFront").attr('src', 'assets/diagrams/Section/or_section.png').stop(true,true).hide().delay(200).fadeIn("slow");
+      $("#diagramFront").attr('src', 'assets/diagrams/Section/03.png').stop(true,true).hide().delay(200).fadeIn("slow");
       $("#sectionBody").text("The diagnostic and treatment areas of the hospital are integrated and shared between the two bed towers. The Kimmel Pavilion surgical reception areas now serve as the front door for all procedure-based care on the campus.");
-      backDiagramURL = 'assets/diagrams/Section/or_section.png';
+      backDiagramURL = 'assets/diagrams/Section/03.png';
       $("#learnMore").removeClass("learnMoreHidden").addClass("learnMoreActive");
       $("#learnMoreLink").attr('href', 'ORPlatform.html');
       $("#s1,#s2,#s4,#s5").removeClass("activeStep");
@@ -86,9 +91,9 @@ $(function () { // wait for document ready
       $("#slideCounter").text("04");
       $("#title").text("Inpatient Units");
       $("#diagramBack").attr('src', backDiagramURL);
-      $("#diagramFront").attr('src', 'assets/diagrams/Section/patientFloors_section.png').stop(true,true).hide().delay(200).fadeIn("slow");
+      $("#diagramFront").attr('src', 'assets/diagrams/Section/04.png').stop(true,true).hide().delay(200).fadeIn("slow");
       $("#sectionBody").text("In designing the nursing units, Ennead worked with NYU Langone to reinvent the processes, technology, and experience of care. The resulting patient room and unit, standardized over the eleven floors, serve as the prototype for inpatient care across the health system.");
-      backDiagramURL = 'assets/diagrams/Section/patientFloors_section.png';
+      backDiagramURL = 'assets/diagrams/Section/04.png';
       $("#learnMore").removeClass("learnMoreHidden").addClass("learnMoreActive");
       $("#learnMoreLink").attr('href', 'inpatientUnits.html');
       $("#s1,#s2,#s3,#s5").removeClass("activeStep");
@@ -105,9 +110,9 @@ $(function () { // wait for document ready
       $("#slideCounter").text("05");
       $("#title").text("Hassenfeld Children's Hospital");
       $("#diagramBack").attr('src', backDiagramURL);
-      $("#diagramFront").attr('src', 'assets/diagrams/Section/hch_section.png').stop(true,true).hide().delay(200).fadeIn("slow");
+      $("#diagramFront").attr('src', 'assets/diagrams/Section/05.png').stop(true,true).hide().delay(200).fadeIn("slow");
       $("#sectionBody").text("Woven through the building is a series of spaces dedicated to children and their families. Dedicated vertical circulation connects the various pediatric spaces and creates a separate and unique experience for children.");
-      backDiagramURL = 'assets/diagrams/Section/hch_section.png';
+      backDiagramURL = 'assets/diagrams/Section/05.png';
       $("#learnMore").removeClass("learnMoreHidden").addClass("learnMoreActive");
       $("#learnMoreLink").attr('href', 'hassenfeld.html');
       $("#s1,#s2,#s3,#s4").removeClass("activeStep");
@@ -119,38 +124,43 @@ $(function () { // wait for document ready
     }
   }
 
-  $('#prevSection').click( function(e) {e.preventDefault(); controller.scrollTo(s1location-1); return false; } );
-  $('#nextSection').click( function(e) {e.preventDefault(); controller.scrollTo(s2location-1); return false; } );
+$('#prevSection').click( function(e) {e.preventDefault(); controller.scrollTo(s1location-1); return false; } );
+$('#nextSection').click( function(e) {e.preventDefault(); controller.scrollTo(s2location-1); return false; } );
 
-  $('#pl1').click( function(e) {e.preventDefault(); controller.scrollTo(s1location-1); return false; } );
-  $('#pl2').click( function(e) {e.preventDefault(); controller.scrollTo(s2location-1); return false; } );
-  $('#pl3').click( function(e) {e.preventDefault(); controller.scrollTo(s3location-1); return false; } );
-  $('#pl4').click( function(e) {e.preventDefault(); controller.scrollTo(s4location-1); return false; } );
-  $('#pl5').click( function(e) {e.preventDefault(); controller.scrollTo(s5location-1); return false; } );
+$('#pl1').click( function(e) {e.preventDefault(); controller.scrollTo(s1location-1); return false; } );
+$('#pl2').click( function(e) {e.preventDefault(); controller.scrollTo(s2location-1); return false; } );
+$('#pl3').click( function(e) {e.preventDefault(); controller.scrollTo(s3location-1); return false; } );
+$('#pl4').click( function(e) {e.preventDefault(); controller.scrollTo(s4location-1); return false; } );
+$('#pl5').click( function(e) {e.preventDefault(); controller.scrollTo(s5location-1); return false; } );
 
-  // build scenes
-  new ScrollMagic.Scene({duration: sectionduration, triggerHook: 0})
+// build scenes
+new ScrollMagic.Scene({duration: sectionduration, triggerHook: 0})
+  .setPin("#block")
+  .on("enter leave", updateBox1)
+  // .addIndicators() // add indicators (requires plugin)
+  .addTo(controller);
+
+new ScrollMagic.Scene({duration: sectionduration, offset: sectionduration, triggerHook: 0})
+  .setPin("#block")
+  .on("enter leave", updateBox2)
+  // .addIndicators() // add indicators (requires plugin)
+  .addTo(controller);
+
+new ScrollMagic.Scene({duration: sectionduration, offset: (sectionduration*2), triggerHook: 0})
+  .setPin("#block")
+  .on("enter leave", updateBox3)
+  // .addIndicators() // add indicators (requires plugin)
+  .addTo(controller);
+
+new ScrollMagic.Scene({duration: sectionduration, offset: (sectionduration*3), triggerHook: 0})
     .setPin("#block")
-    .on("enter leave", updateBox1)
+    .on("enter leave", updateBox4)
     // .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
+});
 
-  new ScrollMagic.Scene({duration: sectionduration, offset: sectionduration, triggerHook: 0})
-    .setPin("#block")
-    .on("enter leave", updateBox2)
-    // .addIndicators() // add indicators (requires plugin)
-    .addTo(controller);
 
-  new ScrollMagic.Scene({duration: sectionduration, offset: (sectionduration*2), triggerHook: 0})
-    .setPin("#block")
-    .on("enter leave", updateBox3)
-    // .addIndicators() // add indicators (requires plugin)
-    .addTo(controller);
-
-  new ScrollMagic.Scene({duration: sectionduration, offset: (sectionduration*3), triggerHook: 0})
-      .setPin("#block")
-      .on("enter leave", updateBox4)
-      // .addIndicators() // add indicators (requires plugin)
-      .addTo(controller);
-
+$( window ).resize(function() {
+  diagramFrontWidth = $("#diagramFront").width()
+  diagramFrontHeight = $("#diagramFront").height();
 });
